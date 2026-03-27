27ead0745030ce13017e30130bbc94bd02259817
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-27 10:52:22 -0500

    style: js-beautify formatting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const proxyPrototype = (proto,handler={}) =>{
+const proxyPrototype = (proto, handler = {}) => {
   const target = Object.getPrototypeOf(proto) ?? {};
-  Object.setPrototypeOf(proto,new Proxy(target,handler));
+  Object.setPrototypeOf(proto, new Proxy(target, handler));
   return proto;
 };
 
-const SmartMap = class SmartMap extends Map{
-  get length(){
+const SmartMap = class SmartMap extends Map {
+  get length() {
     return this.size;
   }
 };
@@ -13,25 +13,25 @@ const SmartMap = class SmartMap extends Map{
 const MapEntries = new Map().entries();
 const MapProto = Map.prototype;
 
-proxyPrototype(SmartMap.prototype,{
-  get(target,key,receiver){
+proxyPrototype(SmartMap.prototype, {
+  get(target, key, receiver) {
     const $this = receiver ?? target;
-    const value = MapProto.get.call($this,key) ?? Reflect.get(...arguments);
-    if(value == undefined && typeof MapEntries[key] === 'function'){
-      
+    const value = MapProto.get.call($this, key) ?? Reflect.get(...arguments);
+    if (value == undefined && typeof MapEntries[key] === 'function') {
+
       return MapEntries[key].bind(MapProto.entries.call($this));
     }
     return value;
   },
-  set(target,key,value,receiver){
+  set(target, key, value, receiver) {
     const $this = receiver ?? target;
-    MapProto.set.call($this,key,value);
+    MapProto.set.call($this, key, value);
     return true
   }
 });
 
-const SmartArray = class SmartArray extends Array{
-  get size(){
+const SmartArray = class SmartArray extends Array {
+  get size() {
     return this.length;
   }
 };
@@ -39,20 +39,20 @@ const SmartArray = class SmartArray extends Array{
 const ArrValues = [].values();
 const ArrProto = Array.prototype;
 
-proxyPrototype(SmartArray.prototype,{
-  get(target,key,receiver){
+proxyPrototype(SmartArray.prototype, {
+  get(target, key, receiver) {
     const $this = receiver ?? target;
     const value = Reflect.get(...arguments);
-    if(value == undefined && typeof ArrValues[key] === 'function'){
-      
+    if (value == undefined && typeof ArrValues[key] === 'function') {
+
       return ArrValues[key].bind(ArrProto.values.call($this));
     }
     return value;
   }
 });
 
-const SmartSet = class SmartSet extends Set{
-  get length(){
+const SmartSet = class SmartSet extends Set {
+  get length() {
     return this.size;
   }
 };
@@ -60,11 +60,11 @@ const SmartSet = class SmartSet extends Set{
 const SetValues = new Set().values();
 const SetProto = Set.prototype;
 
-proxyPrototype(SmartSet.prototype,{
-  get(target,key,receiver){
+proxyPrototype(SmartSet.prototype, {
+  get(target, key, receiver) {
     const $this = receiver ?? target;
     const value = Reflect.get(...arguments);
-    if(value == undefined && typeof SetValues[key] === 'function'){
+    if (value == undefined && typeof SetValues[key] === 'function') {
       return SetValues[key].bind(SetProto.values.call($this));
     }
     return value;
@@ -73,8 +73,8 @@ proxyPrototype(SmartSet.prototype,{
 
 const camelToKebab = str => str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
 
-const SmartHeaders = class SmartHeaders extends Headers{
-  get length(){
+const SmartHeaders = class SmartHeaders extends Headers {
+  get length() {
     return this.size;
   }
 };
@@ -82,42 +82,40 @@ const SmartHeaders = class SmartHeaders extends Headers{
 const HeadEntries = new Headers().entries();
 const HeadProto = Headers.prototype;
 
-const parse = x =>{
-  try{
+const parse = x => {
+  try {
     return JSON.parse(x);
-  }catch{
+  } catch {
     return x;
   }
 };
 
-const stringify = x =>{
-  try{
+const stringify = x => {
+  try {
     return JSON.stringify(x);
-  }catch{
+  } catch {
     return String(x);
   }
 };
 
 const isString = x => typeof x === 'string' || x instanceof String;
 
-
-
-proxyPrototype(SmartHeaders.prototype,{
-  get(target,key,receiver){
+proxyPrototype(SmartHeaders.prototype, {
+  get(target, key, receiver) {
     const $this = receiver ?? target;
-    const value = parse(HeadProto.get.call($this,camelToKebab(String(key)))) ?? Reflect.get(...arguments);
-    if(value == undefined && typeof HeadEntries[key] === 'function'){
-      
+    const value = parse(HeadProto.get.call($this, camelToKebab(String(key)))) ?? Reflect.get(...arguments);
+    if (value == undefined && typeof HeadEntries[key] === 'function') {
+
       return HeadEntries[key].bind(HeadProto.entries.call($this));
     }
     return value;
   },
-  set(target,key,value,receiver){
+  set(target, key, value, receiver) {
     const $this = receiver ?? target;
-    if(!isString(value)){
+    if (!isString(value)) {
       value = stringify(value);
     }
-    HeadProto.set.call($this,camelToKebab(key),value);
+    HeadProto.set.call($this, camelToKebab(key), value);
     return true
   }
 });
